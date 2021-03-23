230a72193f4cdf396175f9887897133fcd393941
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-03-23 16:43:11 -0700

    organized header

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const App: React.FC = () => (
   <div className="app-container">
     <header>
-      <div>
-        Enquizable
-      </div>
+      <Header />
     </header>
     <main>
       Main component
