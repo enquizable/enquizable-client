3a58e4a0d906389153e2917f53929692fceb7ed6
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-03-24 14:45:09 -0700

    made a dummy data file

---
+++ DummyData.ts
@@ -0,0 +1,94 @@
+export default {
+  quizes: [
+    {
+      id: 0,
+      title: 'Hardest problem in the World',
+      questions: [],
+    },
+    {
+      id: 1,
+      title: 'EZ CALP problems',
+      questions: [],
+    },
+    {
+      id: 2,
+      title: 'SYSTEM DESIGNS!',
+      questions: [],
+    },
+    {
+      id: 3,
+      title: 'How many days are in a week',
+      questions: [],
+    },
+    {
+      id: 4,
+      title: 'Kats vs dodge',
+      questions: [],
+    },
+    {
+      id: 5,
+      title: 'JavaScript',
+      questions: [],
+    },
+    {
+      id: 6,
+      title: 'Python',
+      questions: [],
+    },
+    {
+      id: 7,
+      title: 'C++',
+      questions: [],
+    },
+    {
+      id: 8,
+      title: 'Java',
+      questions: [],
+    },
+    {
+      id: 9,
+      title: 'CSS flexbox',
+      questions: [],
+    },
+    {
+      id: 10,
+      title: 'Node.js',
+      questions: [],
+    },
+    {
+      id: 11,
+      title: 'AWS deployment',
+      questions: [],
+    },
+    {
+      id: 12,
+      title: 'React.js is reactive',
+      questions: [],
+    },
+    {
+      id: 13,
+      title: 'jQuery is fun',
+      questions: [],
+    },
+    {
+      id: 14,
+      title: 'HTML for kids',
+      questions: [],
+    },
+    {
+      id: 15,
+      title: 'Async or swim',
+      questions: [],
+    },
+    {
+      id: 16,
+      title: 'Promises are kept forever',
+      questions: [],
+    },
+    {
+      id: 17,
+      title: 'Recursion or die!',
+      questions: [],
+    },
+  ],
+};
