346182ad6fc40cef0ff5666a5f910ad126d6a840
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2021-03-24 12:13:02 -0700

    added search bar, sort button, quiz list

--- client/src/components/BrowseDisplay/BrowseDisplay.tsx
+++ client/src/components/BrowseDisplay/BrowseDisplay.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import BrowseQuizeList from './BrowseQuizeList/BrowseQuizeList';
+import BrowseSearchBar from './BrowseSearchBar/BrowseSearchBar';
 
 const BrowseDisplay: React.FC = () => (
   <div className="browse-display">
-    <div className="browse-display-search-bar-wrapper">
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <div className="browse-display-wrapper">
+      <BrowseSearchBar />
+      <div className="problem-display-wrapper">
+        <BrowseQuizeList />
+      </div>
     </div>
   </div>
 );
---
+++ client/src/components/BrowseDisplay/BrowseQuizeList/BrowseQuizeList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BrowseQuizeList: React.FC = () => (
+  <div>
+    <div>1</div>
+  </div>
+);
+
+export default BrowseQuizeList;
